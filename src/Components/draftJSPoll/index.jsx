f6799908a058fde1f9559c74d4b4a236315a4d19
Sagar Pandey <sagar.pandey@innovaccer.com>
2020-10-27 19:13:24 +0530

    WIP: bar theme

--- src/Components/draftJSPoll/index.jsx
+++ src/Components/draftJSPoll/index.jsx
@@ -1,38 +1,54 @@
+/* eslint-disable react/jsx-wrap-multilines */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable no-tabs */
 import React, { useState } from "react";
 import "./index.scss";
 
 const DraftJSPoll = ({ contentState, block }) => {
-    const [data, setData] = useState({
-		selectedValue: null,
-	});
-
-    const handleChange = (event) => {
-        data.selectedValue = event.target.value
-        setData({ ...data });
-    }
 
     const entityKey = contentState.getLastCreatedEntityKey();
-    const [currData] = useState(contentState.getEntity(entityKey).getData());
+    const [currData, setData] = useState({ ...contentState.getEntity(entityKey).getData(), selectedValue: '' });
+
+    const handleChange = (event) => {
+        currData.selectedValue = event.target.value
+        setData({ ...currData });
+    }
 
     return (
         <>
-        <div className="poll-box">
-            <p className="poll-head">{currData.question}</p>
-            <div className="options">
-                {
-                    currData.options.map((elem, idx) => (
-                    <ul key={idx}>
-                        <div className="mask">
-                            <input type="radio" name={`poll-${block.getKey()}`} value={elem.value} onChange={(e) => handleChange(e)} /> 
-                            <span className="option pl-10">{elem.value}</span>
+        { currData.pollTheme === 'Bar Chart' 
+            && (<div className="poll-progress-box">
+                <p className="poll-head text-align-center">{currData.question}</p>
+                <div className="options">
+                    {
+                        currData.options.map((elem, idx) => (
+                        <div className="bar" value={elem.value} onClick={(e) => handleChange(e)}>
+                            <div className="bar-fill" style={{'width' : '20%'}}>
+                                <span className="bar-label">{`${20}%  `}{elem.value}</span>
+                            </div>
                         </div>
-                    </ul>
-                    ))
-                }
-            </div>
-        </div>
+                        ))
+                    }
+                </div>
+             </div>
+            )}
+        { currData.pollTheme === 'Option List' 
+            && (<div className="poll-box">
+                <p className="poll-head">{currData.question}</p>
+                <div className="options">
+                    {
+                        currData.options.map((elem, idx) => (
+                        <ul key={idx}>
+                            <div className="mask">
+                                <input type="radio" name={`poll-${block.getKey()}`} value={elem.value} onChange={(e) => handleChange(e)} /> 
+                                <span className="option pl-10">{elem.value}</span>
+                            </div>
+                        </ul>
+                        ))
+                    }
+                </div>
+             </div>
+            )}
         </>
      )
     }
